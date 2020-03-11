d17fbf9a543e32cf790a2d3fdf4908346a39d06b
Jaroslav Jindrich <jajindri@microsoft.com>
2020-03-11 17:02:40 +0100

    powershell deploy tf

---
+++ src-jjaks/deploy.ps1
@@ -0,0 +1,21 @@
+# powershell
+echo "Setting environment variables for Terraform"
+$tenant=$(az account show -o tsv --query tenantId)
+$subscription=$(az account show -o tsv --query id)
+
+# az ad sp create-for-rbac -n jjtf --role="Contributor" --scopes="/subscriptions/${subscription}"
+$clientApplicationId=$(az keyvault secret show --vault-name jjkeyvault --name tfClientApplicationId -o tsv --query value)
+$clientSecret=$(az keyvault secret show --vault-name jjkeyvault --name tfClientSecret -o tsv --query value)
+
+$Env:ARM_SUBSCRIPTION_ID=$subscription
+$Env:ARM_TENANT_ID=$tenant
+$Env:ARM_CLIENT_ID=$clientApplicationId
+$Env:ARM_CLIENT_SECRET=$clientSecret
+
+# https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_windows_amd64.zip
+
+echo "Running Terraform deployment"
+terraform init
+terraform plan
+
+terraform apply -auto-approve
