791b2528860f385b0bf31c48c707c58921d9572c
Jaroslav Jindrich <jajindri@microsoft.com>
2020-03-13 12:13:15 +0100

    fix AKS admin config

--- src-jjaks/aks-kubernetes.tf
+++ src-jjaks/aks-kubernetes.tf
@@ -2,10 +2,10 @@
 provider "kubernetes" {
   version                = "~> 1.11"
   load_config_file       = false
-  host                   = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.k8s.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.cluster_ca_certificate)
 }
 
 # Grant cluster-admin rights to the default service account
