b8d83779a144dbb73b57605a6f28c228b2c727cf
Jaroslav Jindrich <jajindri@microsoft.com>
2020-12-22 12:29:19 +0100

    fix helm

--- src-jjaks/aks-helm.tf
+++ src-jjaks/aks-helm.tf
@@ -2,7 +2,6 @@
 provider "helm" {
   version = "~> 2.0"
   kubernetes {
-    load_config_file       = false
     host                   = azurerm_kubernetes_cluster.k8s.kube_admin_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_key)
