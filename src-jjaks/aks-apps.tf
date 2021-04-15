6fdc739ef39b577051be9a21a0085562fdfb3819
Jaroslav Jindrich <jajindri@microsoft.com>
2021-04-15 11:35:39 +0200

    jjaks namespaces jjweb, jjapi

---
+++ src-jjaks/aks-apps.tf
@@ -0,0 +1,15 @@
+# resources for applications
+
+# namespaces for JJWeb application
+resource "kubernetes_namespace" "jjweb-ns" {
+  metadata {
+    name = "jjweb"
+  }
+  depends_on = [azurerm_kubernetes_cluster.k8s]
+}
+resource "kubernetes_namespace" "jjapi-ns" {
+  metadata {
+    name = "jjapi"
+  }
+  depends_on = [azurerm_kubernetes_cluster.k8s]
+}
