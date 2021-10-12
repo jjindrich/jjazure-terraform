eef59ce86319e9b6919342e70c225af5e11589cc
Jaroslav Jindrich <jajindri@microsoft.com>
2021-10-12 15:31:32 +0200

    TF fix variables

--- src-jjaks/aks.variable.tf
+++ src-jjaks/aks.variable.tf
@@ -1,8 +1,8 @@
-variable resource_group_name {}
-variable resource_group_node_name {}
-variable cluster_name {}
-variable aad_aks_admin_role {}
-variable ingress_load_balancer_ip {}
+variable "resource_group_name" {}
+variable "resource_group_node_name" {}
+variable "cluster_name" {}
+variable "aad_aks_admin_role" {}
+variable "ingress_load_balancer_ip" {}
 variable "keyvault_name" {}
 variable "keyvault_rg" {}
 variable "acr_name" {}
