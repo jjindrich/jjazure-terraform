e98287f16768ff13b008d19ebdd312b8aba599a0
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-10-08 10:42:03 +0200

    User MI ID fix

--- src-jjaks/main.tf
+++ src-jjaks/main.tf
@@ -143,7 +143,7 @@ resource "azurerm_key_vault_access_policy" "kv_access_current" {
 resource "azurerm_key_vault_access_policy" "kv_access_app" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_user_assigned_identity.identity_appdeploy.client_id
+  object_id    = data.azurerm_user_assigned_identity.identity_appdeploy.id
   secret_permissions = [
     "Set",
     "Get",
