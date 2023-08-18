9d67e83c6b9f5af0e21848e5013059c03c5cba27
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2023-08-18 11:14:54 +0200

    prometheus rules

--- src-jjaks/aks-apps.tf
+++ src-jjaks/aks-apps.tf
@@ -3,11 +3,21 @@ resource "kubernetes_namespace" "jjweb-ns" {
   metadata {
     name = "jjweb"
   }
+  lifecycle {
+    ignore_changes = [
+      metadata,
+    ]
+  }
   depends_on = [azurerm_kubernetes_cluster.k8s]
 }
 resource "kubernetes_namespace" "jjapi-ns" {
   metadata {
     name = "jjapi"
+  }
+  lifecycle {
+    ignore_changes = [
+      metadata,
+    ]
   }
   depends_on = [azurerm_kubernetes_cluster.k8s]
 }
